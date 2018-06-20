8279974a8521083b06e4298327e2a455b443d33c
Yatheen07 <pravan.yatheen@gmail.com>
2018-06-20 14:06:06 +0530

    Trained Cat vs Dog classifier

---
+++ classifier.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 17:03:32 2018
+
+@author: yatheen!
+    
+"""
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+model = Sequential()
+
+# Convolution
+model.add(Conv2D(32, (3, 3), input_shape = (50, 50, 3), activation = 'relu'))
+
+# Pooling
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Second convolutional layer
+model.add(Conv2D(32, (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Flattening
+model.add(Flatten())
+
+# Full connection
+model.add(Dense(units = 128, activation = 'relu'))
+model.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+#test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory(r'E:\Mini Projects\Machine Learning\CNN\Dataset\training_set',
+                                                 target_size = (50, 50),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+model.fit_generator(training_set,
+                         steps_per_epoch = 4000,
+                         epochs = 25,
+                         validation_steps = 2000)
+
+model_json = model.to_json()
+with open("./model.json","w") as json_file:
+  json_file.write(model_json)
+
+model.save_weights("./model.h5")
+print("saved model..! ready to go.")
---
+++ trained_Model.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 13:09:22 2018
+
+@author: yatheen!
+"""
+
+from keras.models import model_from_json
+import cv2
+import numpy as np
+
+
+json_file = open('./cat or dog, Image classification using CNN/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+loaded_model.load_weights("./cat or dog, Image classification using CNN/model.h5")
+print("Loaded model from disk")
+
+'''loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+'''
+loaded_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+img = cv2.imread('./Dataset/dog_test2.jpg')
+print(img)
+img = cv2.resize(img, (50,50))
+print(img.shape)
+img = img.reshape(1, 50, 50, 3)
+
+print(img.shape)
+#print(np.argmax(loaded_model.predict(img)))
+result = loaded_model.predict_classes(img)
+print()
+
+if(result[0][0] == 1):
+    print("Hey Doggie")
+else:
+    print("Hey cattie")
