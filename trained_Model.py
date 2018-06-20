d342dda7a473d99537e7095bd05c6d5d35f4e637
Yatheen07 <pravan.yatheen@gmail.com>
2018-06-20 15:23:23 +0530

    trained_model.py completed

--- trained_Model.py
+++ trained_Model.py
@@ -5,34 +5,32 @@ Created on Wed Jun 20 13:09:22 2018
 @author: yatheen!
 """
 
+# Step 1: Import the packages
 from keras.models import model_from_json
 import cv2
 import numpy as np
 
-
+# Step 2: Load the Model from Json File
 json_file = open('./model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 
+# Step 3: Load the weights
 loaded_model.load_weights("./model.h5")
 print("Loaded model from disk")
 
-
+# Step 4: Compile the model
 loaded_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+# Step 5: load the image you want to test
 img = cv2.imread('./Dataset/dog_test2.jpg')
-print(img)
 img = cv2.resize(img, (50,50))
-print(img.shape)
 img = img.reshape(1, 50, 50, 3)
 
-print(img.shape)
-#print(np.argmax(loaded_model.predict(img)))
+# Step 6: Predict to which class your input image has been classified
 result = loaded_model.predict_classes(img)
-print()
-
 if(result[0][0] == 1):
-    print("I guess this is a Dog!")
+    print("I guess this must be a Dog!")
 else:
-    print("I guess this is a Cat!")
+    print("I guess this must be a Cat!")
